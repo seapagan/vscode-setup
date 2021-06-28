cb9bbd099058af6bd4561d4cc45686753236c95c
Grant Ramsay <seapagan@gmail.com>
2021-06-28 11:41:01 +0100

    add python script to create a extensions.json
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

---
+++ make-extension-config.py
@@ -0,0 +1,20 @@
+"""
+Create a 'global.extensions.json' file based on a test file listing
+VSCode extensions, this should be renamed to 'extensions.json' and placed in
+the final vscode directiory
+"""
+
+import json
+
+EXTENSION_LIST = "raw-extension-list.txt"
+OUTPUT_JSON = "global.extensions.json"
+
+
+output_dict = {"recomendations": []}
+
+with open(EXTENSION_LIST, "r") as file:
+    for line in file:
+        output_dict["recomendations"].append(line.strip())
+
+with open(OUTPUT_JSON, "w") as file:
+    file.write(json.dumps(output_dict, indent=4))
