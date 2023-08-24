6fea83b6b074ee2c6e0c18990a936be32c5b590d
Grant Ramsay <seapagan@gmail.com>
2023-08-24 10:02:32 +0100

    tidy scripts and fix getting config from vscode
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- make-extension-config.py
+++ make-extension-config.py
@@ -8,14 +8,12 @@ import json
 import subprocess
 
 OUTPUT_JSON = ".insiders-extensions.json"
-CODE_VERSION = "code"
+CODE_VERSION = "code-insiders"
 
 # get a dump of the installed extensions from the local vcsode installation
 result = (
     subprocess.run(
-        [CODE_VERSION, "--list-extensions"],
-        stdout=subprocess.PIPE,
-        shell=True,
+        [CODE_VERSION, "--list-extensions"], stdout=subprocess.PIPE, check=True
     )
     .stdout.decode("utf-8")
     .splitlines()
@@ -26,10 +24,10 @@ print(f"Found {len(result)} extensions installed.")
 # if remote, the first line is the name of the environment we're running this
 # on.
 if "Extensions installed" in result[0]:
-    code_environment = result.pop(0).split(":")[1].strip()
+    CODE_ENVIRONMENT = result.pop(0).split(":")[1].strip()
 else:
-    code_environment = "global"
-print(f"Saving extensions.json for the '{code_environment}' environment.")
+    CODE_ENVIRONMENT = "global"
+print(f"Saving extensions.json for the '{CODE_ENVIRONMENT}' environment.")
 
 output_dict: dict[str, list[str]] = {"recommendations": []}
 
@@ -37,5 +35,5 @@ for extension in result:
     output_dict["recommendations"].append(extension.strip())
 
 
-with open(f"{code_environment}{OUTPUT_JSON}", "w") as file:
+with open(f"{CODE_ENVIRONMENT}{OUTPUT_JSON}", "w", encoding="utf-8") as file:
     file.write(json.dumps(output_dict, indent=2))
--- make-extension-from-file.py
+++ make-extension-from-file.py
@@ -7,16 +7,16 @@ import json
 
 OUTPUT_JSON = ".insiders-extensions.json"
 
-with open("extensions.list") as file:
+with open("extensions.list", encoding="utf-8") as file:
     result = file.read().splitlines()
 
 # if remote, the first line is the name of the environment we're running this
 # on.
 if "Extensions installed" in result[0]:
-    code_environment = result.pop(0).split(":")[1].strip()
+    CODE_ENVIRONMENT = result.pop(0).split(":")[1].strip()
 else:
-    code_environment = "global"
-print(f"Saving extensions.json for the '{code_environment}' environment.")
+    CODE_ENVIRONMENT = "global"
+print(f"Saving extensions.json for the '{CODE_ENVIRONMENT}' environment.")
 
 output_dict: dict[str, list[str]] = {"recommendations": []}
 
@@ -24,5 +24,5 @@ for extension in result:
     output_dict["recommendations"].append(extension.strip())
 
 
-with open(f"{code_environment}{OUTPUT_JSON}", "w") as file:
+with open(f"{CODE_ENVIRONMENT}{OUTPUT_JSON}", "w", encoding="utf-8") as file:
     file.write(json.dumps(output_dict, indent=2))
